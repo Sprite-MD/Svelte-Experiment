86985b57c6c7183b5942cce5dbf369df0e945a83
Sprite <ponkaew.wasin@gmail.com>
2025-08-20 13:50:18 -0500

    minor addition to task items

--- experiment/src/lib/components/TaskItem.svelte
+++ experiment/src/lib/components/TaskItem.svelte
@@ -4,5 +4,8 @@
     export let onDelete;
 </script>
 
-
-
+<div>
+    <input type="checkbox" checked={task.done} on:change={() => onToggle(task.id)} />
+    <span style:text-decoration={task.done ? 'line-through' : 'none'}>{task.title}</span>
+    <button on:click={() => onDelete(task.id)}>"delete"</button>
+</div>
