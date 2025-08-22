61b5bf29b12b56d34ab89377849192b5f882c43c
Sprite <ponkaew.wasin@gmail.com>
2025-08-22 01:24:03 -0500

    moved store file and added changes to page.svelte

--- experiment/src/routes/+page.svelte
+++ experiment/src/routes/+page.svelte
@@ -1,5 +1,11 @@
 <script>
-    // Code
+    import {tasks} from '$lib/stores/tasks.js'
+    import TaskForm from '$lib/components/TaskForm.svelte'
+    import TaskItem from '$lib/components/TaskItem.svelte';
 </script>
 
+function addTask(event) {
+    const title = event.detail.title;
+
+}
 <h1>Task Tracker</h1>
